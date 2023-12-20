b2fe702f4d5833a96656792b6b4d26702e291cce
Meihao-Han <meihao.hsu@gmail.com>
2023-12-20 15:54:48 +0800

    牌組列表調整HTML觸發

--- resources/views/myDeck.blade.php
+++ resources/views/myDeck.blade.php
@@ -3,21 +3,23 @@
 
 <div id="my-deck" class="m-4 m-lg-5">
     @foreach($decks as $deck)
-        <div class="row deck-list bg-yellow rounded-3 p-3 mb-4 align-items-center">
-            <div class="col-sm-9 mb-2">
-                <div class="d-flex flex-wrap">
-                    <div class="deck-title">{{$deck->title}}</div>
-                    <a href="" class="btn deck-btn">{{$deck->code}}<i class="fa fa-sign-out ms-2"></i>
-                    </a>
-                </div>
+        <a class="text-decoration-none text-dark" href="{{route('myAccount.myDeckDetail',['deck',$deck->id])}}">
+            <div class="row deck-list bg-yellow rounded-3 p-3 mb-4 align-items-center">
+                <div class="col-sm-9 mb-2">
+                    <div class="d-flex flex-wrap">
+                        <div class="deck-title">{{$deck->title}}</div>
+                        <span class="btn deck-btn">{{$deck->code}}<i class="fa fa-sign-out ms-2"></i>
+                        </span>
+                    </div>
 
-                <p class="deck-date m-0">{{$deck->created_at->format('Y/m/d')}}</p>
+                    <p class="deck-date m-0">{{$deck->created_at->format('Y/m/d')}}</p>
+                </div>
+                <div class="col-sm-3 text-sm-end">
+                    <div class="deck-number">{{$deck->deckBuildCategoryTotal()['total']}}張</div>
+                    <button class="deck-del btn-text bg-yellow border-0">刪除</button>
+                </div>
             </div>
-            <div class="col-sm-3 text-sm-end">
-                <div class="deck-number">{{$deck->deckBuildCategoryTotal()['total']}}張</div>
-                <a href="" class="deck-del btn-text">刪除</a>
-            </div>
-        </div>
+        </a>
     @endforeach
 
         <div class="frontend-pagination">
@@ -27,4 +29,9 @@
 
 @endsection
 @push('app-scripts')
+    <script>
+        $('.deck-btn, .deck-del').on('click',function (e) {
+            e.preventDefault();
+        })
+    </script>
 @endpush
